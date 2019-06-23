bbe221857b01aae685ad27aeee805e78878ae501
Mohamed Saif (B2) <mohamed.saif@outlook.com>
2019-06-23 16:09:03 +0400

    Added few Utlis :)

---
+++ src/CognitivePipeline/CognitivePipeline.Functions/Utils/FuncExecuter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitivePipeline.Functions.Utils
+{
+    /// <summary>
+    /// Utility class to help with getting both environment and Azure Key Vault settings
+    /// </summary>
+    public class FuncExecuter
+    {
+        public static HttpClient httpClient = new HttpClient();
+        private static string functionKey = GlobalSettings.GetKeyValue("FunctionGlobalKey");
+
+        public static async Task<IActionResult> CallFunction(string functionUri, StringContent content)
+        {
+            var result = "";
+            httpClient.DefaultRequestHeaders.Clear();
+            httpClient.DefaultRequestHeaders.Add("x-functions-key", functionKey);
+
+            using (HttpResponseMessage response = (await httpClient.PostAsync(functionUri, content)))
+            using (HttpContent responseContent = response.Content)
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    result = await responseContent.ReadAsStringAsync();
+                }
+                else
+                {
+                    //TODO: Handle the non 200 code response messages here
+
+                    return (ActionResult)new BadRequestObjectResult(result);
+                }
+            }
+
+            return (ActionResult)new OkObjectResult(result);
+        }
+    }
+}
---
+++ src/CognitivePipeline/CognitivePipeline.Functions/Utils/GlobalSettings.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitivePipeline.Functions.Utils
+{
+    public class GlobalSettings
+    {
+        //https://integration.team/2017/09/25/retrieve-azure-key-vault-secrets-using-azure-functions-managed-service-identity/
+        //Declaring static fields will allow reusability without creating them everytime
+        static HttpClient client = new HttpClient();
+        static AzureServiceTokenProvider azureServiceTokenProvider;
+        static KeyVaultClient kvClient;
+
+        public static string GetEnvironmentVariable(string name)
+        {
+            return System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        }
+
+        public static async Task<string> GetKeyVaultSecret(string secretName)
+        {
+            //Don't forget to provision a service identity on your function. Refer to documentation for further information
+            azureServiceTokenProvider = new AzureServiceTokenProvider();
+
+            kvClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback), client);
+
+            string secretValue = (await kvClient.GetSecretAsync(Environment.GetEnvironmentVariable(secretName)))?.Value;
+
+            return secretValue;
+        }
+
+        public static async Task<string> GetKeyValue(string keyName, bool isSecret)
+        {
+            if (isSecret)
+                return await GetKeyVaultSecret(keyName);
+
+            return GetEnvironmentVariable(keyName);
+        }
+
+        public static string GetKeyValue(string keyName)
+        {
+            bool isSecret = GetEnvironmentVariable("Env") == "Protected";
+            if (isSecret)
+            {
+                //return await GetKeyVaultSecret(keyName);
+                Task<string> t = GetKeyVaultSecret(keyName);
+                t.Wait();
+                return t.Result;
+            }
+            return GetEnvironmentVariable(keyName);
+        }
+    }
+}
