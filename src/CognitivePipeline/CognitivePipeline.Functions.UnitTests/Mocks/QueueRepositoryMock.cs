58c260a85e57bbfe1bc53e397e2a017847e60563
Mohamed Saif (B2) <mohamed.saif@outlook.com>
2019-06-24 11:45:38 +0400

    Added mock for the queue repo

---
+++ src/CognitivePipeline/CognitivePipeline.Functions.UnitTests/Mocks/QueueRepositoryMock.cs
@@ -0,0 +1,35 @@
+﻿using CognitivePipeline.Functions.Abstractions;
+using CognitivePipeline.Functions.UnitTests.Helpers;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitivePipeline.Functions.UnitTests.Mocks
+{
+    public class QueueRepositoryMock : IQueueRepository
+    {
+        public Task<bool> DeleteMessage(CloudQueueMessage message)
+        {
+            return Task.FromResult<bool>(true);
+        }
+
+        public Task<CloudQueueMessage> GetMessage()
+        {
+            return Task.FromResult<CloudQueueMessage>(new CloudQueueMessage(JsonConvert.SerializeObject(TestFactory.CognitiveFileDTOs[0])));
+        }
+
+        public Task<int> GetQueueLength()
+        {
+            return Task.FromResult<int>(1);
+        }
+
+        public Task<bool> QueueMessage(string message)
+        {
+            return Task.FromResult<bool>(true);
+        }
+    }
+}
